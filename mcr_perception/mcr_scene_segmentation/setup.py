4e44498e7202c8be77f0de7e013a9ca7a669f709
demo@youbot-brsu-2-pc1 <demo@youbot-brsu-2-pc1>
2014-03-25 10:15:24 +0100

    fix package name in setup.py

--- mcr_perception/mcr_scene_segmentation/setup.py
+++ mcr_perception/mcr_scene_segmentation/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['mcr_scene_segmentation_'],
+    packages=['mcr_scene_segmentation'],
     package_dir={'mcr_scene_segmentation' : 'ros/src'}
 )
 
