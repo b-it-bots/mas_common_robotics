c28b5534ae5d887aeab4240a4068ed916e7c2ea1
Frederik Hegger <frederik.hegger@h-brs.de>
2015-03-29 00:23:08 +0100

    [common converters] add node to retrieve motion direction from Twist

---
+++ mcr_common/mcr_common_converters/ros/src/twist_to_motion_direction_conversion_node.cpp
@@ -0,0 +1,59 @@
+/*
+ * twist_to_motion_direction_conversion_node.cpp
+ *
+ *  Created on: Mar 28, 2015
+ *      Author: fred
+ */
+
+#include <mcr_common_converters/twist_to_motion_direction_conversion_node.h>
+
+TwistToMotionDirectionConversionNode::TwistToMotionDirectionConversionNode()
+{
+    ros::NodeHandle nh("~");
+
+    sub_twist_ = nh.subscribe("input/twist", 1, &TwistToMotionDirectionConversionNode::twistCallback, this);
+    pub_motion_direction_ = nh.advertise<geometry_msgs::PoseStamped>("output/pose", 1);
+
+    nh.param < std::string > ("frame_id", frame_id_, "/base_link");
+}
+
+TwistToMotionDirectionConversionNode::~TwistToMotionDirectionConversionNode()
+{
+    sub_twist_.shutdown();
+    pub_motion_direction_.shutdown();
+}
+
+void TwistToMotionDirectionConversionNode::twistCallback(const geometry_msgs::TwistPtr &msg)
+{
+    if(pub_motion_direction_.getNumSubscribers() <= 0)
+        return;
+
+    double motion_direction = 0.0;
+    geometry_msgs::PoseStamped pose;
+
+    motion_direction = getMotionDirectionFromTwist2D(msg->linear.x, msg->linear.y, msg->angular.z);
+
+    pose.header.stamp = ros::Time::now();
+    pose.header.frame_id = frame_id_;
+
+    pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, motion_direction);
+
+    pub_motion_direction_.publish(pose);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "twist_to_motion_direction_conversion");
+    ros::NodeHandle nh("~");
+
+    TwistToMotionDirectionConversionNode conversion_node = TwistToMotionDirectionConversionNode();
+
+    ros::Rate loop_rate(30);
+
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
