2214269450652e6278a86433379c34926c937768
haramoz <arkamallick30@gmail.com>
2015-04-20 16:31:09 +0200

    Implemented twist demultiplexer to separate arm and base twist

---
+++ mcr_controllers/mcr_twist_demultiplexer/ros/include/mcr_twist_demultiplexer/twist_demultiplexer_node.h
@@ -0,0 +1,61 @@
+#ifndef TWIST_DEMULTIPLEXER_NODE_H_
+#define TWIST_DEMULTIPLEXER_NODE_H_
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <mcr_geometry_transformer/geometry_transformer.h>
+
+
+class TwistDemultiplexerNode
+{
+
+    public:
+        TwistDemultiplexerNode(ros::NodeHandle &nh);
+        virtual ~TwistDemultiplexerNode();
+        void eventCallback(const std_msgs::String &event_command);
+        void twistStampedCallback(const geometry_msgs::TwistStamped &msg);
+        void states();
+        void initState();
+        void idleState();
+        void runState();
+
+    private:
+        enum States {
+            INIT,
+            IDLE,
+            RUNNING
+        };
+
+
+    private:
+        void demultiplexTwist();
+
+
+    private:
+        ros::NodeHandle node_handler_;
+
+        ros::Subscriber event_sub_;
+        ros::Subscriber twist_sub_;
+
+        ros::Publisher event_pub_;
+        ros::Publisher arm_twist_stamped_pub_;
+        ros::Publisher base_twist_pub_;
+
+        geometry_msgs::TwistStamped input_twist_;
+
+        bool start_twist_demultiplexer_;
+        bool twist_sub_status_;
+
+        std::string base_tf_;
+        std::string arm_tf_;
+
+        GeometryTransformer geometry_transformer_;
+
+        States run_state_;
+
+};
+
+#endif
---
+++ mcr_controllers/mcr_twist_demultiplexer/ros/src/twist_demultiplexer_node.cpp
@@ -0,0 +1,152 @@
+#include <mcr_twist_demultiplexer/twist_demultiplexer_node.h>
+
+TwistDemultiplexerNode::TwistDemultiplexerNode(ros::NodeHandle &nh) : node_handler_(nh)
+{
+    node_handler_.getParam("base_tf_", base_tf_);
+    node_handler_.getParam("arm_tf_", arm_tf_);
+
+    event_pub_ = node_handler_.advertise<std_msgs::String>("event_out", 1);
+    arm_twist_stamped_pub_ = node_handler_.advertise<geometry_msgs::TwistStamped>("arm_twist", 1);
+    base_twist_pub_ = node_handler_.advertise<geometry_msgs::Twist>("base_twist", 1);
+
+    event_sub_ = node_handler_.subscribe("event_in", 1, &TwistDemultiplexerNode::eventCallback, this);
+    twist_sub_ = node_handler_.subscribe("input_twist", 1, &TwistDemultiplexerNode::twistStampedCallback, this);
+
+    run_state_ = INIT;
+    start_twist_demultiplexer_ = false;
+    twist_sub_status_ = false;
+}
+
+TwistDemultiplexerNode::~TwistDemultiplexerNode()
+{
+    event_sub_.shutdown();
+    twist_sub_.shutdown();
+    arm_twist_stamped_pub_.shutdown();
+    base_twist_pub_.shutdown();
+    event_pub_.shutdown();
+}
+
+void TwistDemultiplexerNode::eventCallback(const std_msgs::String &event_command)
+{
+    if (event_command.data == "e_start") {
+        start_twist_demultiplexer_ = true;
+        ROS_INFO("Twist Demultiplexer ENABLED");
+    } else if (event_command.data == "e_stop") {
+        start_twist_demultiplexer_ = false;
+        ROS_INFO("Twist Demultiplexer DISABLED");
+    }
+}
+
+void TwistDemultiplexerNode::twistStampedCallback(const geometry_msgs::TwistStamped &msg)
+{
+    input_twist_ = msg;
+    twist_sub_status_ = true;
+}
+
+void TwistDemultiplexerNode::states()
+{
+    switch (run_state_) {
+        case INIT:
+            initState();
+            break;
+        case IDLE:
+            idleState();
+            break;
+        case RUNNING:
+            runState();
+            break;
+        default:
+            initState();
+    }
+}
+
+void TwistDemultiplexerNode::initState()
+{
+    if (twist_sub_status_) {
+        run_state_ = IDLE;
+        twist_sub_status_ = false;
+    }
+}
+
+void TwistDemultiplexerNode::idleState()
+{
+    if (start_twist_demultiplexer_) {
+        run_state_ = RUNNING;
+    } else {
+        run_state_ = INIT;
+    }
+}
+
+void TwistDemultiplexerNode::runState()
+{
+    demultiplexTwist();
+    run_state_ = INIT;
+}
+
+void TwistDemultiplexerNode::demultiplexTwist()
+{
+    // copy input twist message
+    geometry_msgs::TwistStamped arm_twist(input_twist_);
+    geometry_msgs::TwistStamped base_twist(input_twist_);
+
+    // transform twists to required frames
+    geometry_msgs::TwistStamped transformed_arm_twist = geometry_transformer_.transformTwist(arm_tf_, arm_twist);
+    geometry_msgs::TwistStamped transformed_base_twist = geometry_transformer_.transformTwist(base_tf_, base_twist);
+
+    // set x,y angular arm motions to zero
+    transformed_arm_twist.twist.angular.x = 0.0;
+    transformed_arm_twist.twist.angular.y = 0.0;
+
+    // set z linear arm motions to zero
+    transformed_arm_twist.twist.linear.z = 0.0;
+
+    // set angular base motions to zero and linear z to zero
+    transformed_base_twist.twist.angular.x = 0.0;
+    transformed_base_twist.twist.angular.y = 0.0;
+    transformed_base_twist.twist.angular.z = 0.0;
+
+    transformed_base_twist.twist.linear.z = 0.0;
+
+    double minimum_base_velocity_x = 0.0;
+    double minimum_base_velocity_y = 0.0;
+
+    node_handler_.getParam("minimum_base_velocity_x", minimum_base_velocity_x);
+    node_handler_.getParam("minimum_base_velocity_y", minimum_base_velocity_y);
+
+    // if base velocity is too low, move the arm instead
+    if (std::abs(transformed_base_twist.twist.linear.x) < minimum_base_velocity_x)
+    {
+        transformed_base_twist.twist.linear.x = 0.0;
+    }
+    else
+    {
+        transformed_arm_twist.twist.linear.x = 0.0;
+    }
+
+    if (std::abs(transformed_base_twist.twist.linear.y) < minimum_base_velocity_y)
+    {
+        transformed_base_twist.twist.linear.y = 0.0;
+    }
+    else
+    {
+        transformed_arm_twist.twist.linear.y = 0.0;
+    }
+
+    arm_twist_stamped_pub_.publish(transformed_arm_twist);
+    base_twist_pub_.publish(transformed_base_twist.twist);
+
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "twist_demultiplexer");
+    ros::NodeHandle nh("~");
+    ROS_INFO("[twist demultiplexer] node started");
+    TwistDemultiplexerNode twist_demultiplexer_node(nh);
+    while (ros::ok()) {
+        twist_demultiplexer_node.states();
+        ros::spinOnce();
+    }
+    return 0;
+}
