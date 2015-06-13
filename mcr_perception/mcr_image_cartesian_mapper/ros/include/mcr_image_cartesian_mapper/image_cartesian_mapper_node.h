bcf7415f58e668e07e398a1eae9fbdcdf91b1e55
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2015-06-13 17:38:32 +0200

    Updated to map from image to cartesian frame

--- mcr_perception/mcr_image_cartesian_mapper/ros/include/mcr_image_cartesian_mapper/image_cartesian_mapper_node.h
+++ mcr_perception/mcr_image_cartesian_mapper/ros/include/mcr_image_cartesian_mapper/image_cartesian_mapper_node.h
@@ -9,20 +9,12 @@
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/String.h>
-#include <std_msgs/UInt32.h>
-#include <tf/transform_datatypes.h>
 #include <string>
-#include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
-#include <opencv2/opencv.hpp>
+#include <sensor_msgs/CameraInfo.h>
+#include <tf/transform_listener.h>
+#include <Eigen/LU>
 
 #define PI 3.14159265
-
-using namespace cv;
-
 
 class ImageCartesianMapperNode
 {
@@ -31,13 +23,15 @@ class ImageCartesianMapperNode
         ImageCartesianMapperNode(ros::NodeHandle &nh);
         virtual ~ImageCartesianMapperNode();
         void eventCallback(const std_msgs::String &event_command);
-        void imageCallback(const sensor_msgs::ImageConstPtr &image);
         void poseCallback(const geometry_msgs::Pose2D::ConstPtr &pose);
+        void cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr &camera_info);
+        
         void states();
         void initState();
         void idleState();
         void runState();
-        void imagetoCartesianMapper();
+        bool cameraOpticalToCameraMetrical();
+        bool cameraMetricalToCartesian();
 
     private:
         enum States {
@@ -50,20 +44,24 @@ class ImageCartesianMapperNode
         ros::NodeHandle node_handler_;
         ros::Subscriber event_sub_;
         ros::Subscriber pose_sub_;
-        image_transport::Subscriber image_sub_;
-        sensor_msgs::ImageConstPtr image_message_;
-        image_transport::ImageTransport image_transporter_;
+        ros::Subscriber camera_info_sub_;
+        sensor_msgs::CameraInfoConstPtr camera_info_;
         ros::Publisher event_pub_;
         ros::Publisher cartesian_pub_;
-        bool image_sub_status_;
         bool pose_sub_status_;
         bool start_cartesian_mapper_;
+        bool camera_info_sub_status_;
         geometry_msgs::Pose2D pose_2d_;
+        geometry_msgs::PoseStamped camera_optical_pose_;
         geometry_msgs::PoseStamped cartesian_pose_;
-        int center_x_;
-        int center_y_;
-        std_msgs::String status_msg_;
         States run_state_;
+        tf::TransformListener listener_;
+        std::string target_frame_;
+        std::string source_frame_;
+        Eigen::Vector3f camera_coordinates_;
+        Eigen::Matrix3f camera_intrinsic_matrix_;
+        Eigen::Vector3f camera_metrical_coordinates_;
+        std_msgs::String event_out_msg_;
 
 };
 
--- mcr_perception/mcr_image_cartesian_mapper/ros/src/image_cartesian_mapper_node.cpp
+++ mcr_perception/mcr_image_cartesian_mapper/ros/src/image_cartesian_mapper_node.cpp
@@ -1,37 +1,40 @@
 #include <mcr_image_cartesian_mapper/image_cartesian_mapper_node.h>
 
-ImageCartesianMapperNode::ImageCartesianMapperNode(ros::NodeHandle &nh) : node_handler_(nh), image_transporter_(nh)
+ImageCartesianMapperNode::ImageCartesianMapperNode(ros::NodeHandle &nh) : node_handler_(nh)
 {
     event_sub_ = node_handler_.subscribe("event_in", 1, &ImageCartesianMapperNode::eventCallback, this);
     pose_sub_ = node_handler_.subscribe("pose", 1, &ImageCartesianMapperNode::poseCallback, this);
-    image_sub_ = image_transporter_.subscribe("image", 1, &ImageCartesianMapperNode::imageCallback, this);
+    camera_info_sub_ = node_handler_.subscribe("camera_info", 1, &ImageCartesianMapperNode::cameraInfoCallback, this);
     cartesian_pub_ = node_handler_.advertise<geometry_msgs::PoseStamped>("cartesian_pose", 1);
     event_pub_ = node_handler_.advertise<std_msgs::String>("event_out", 1);
+    target_frame_ = "base_link";
+    node_handler_.getParam("target_frame", target_frame_);
+    source_frame_ = "tower_cam3d_rgb_optical_frame";
+    node_handler_.getParam("source_frame", source_frame_);
     run_state_ = INIT;
     start_cartesian_mapper_ = false;
     pose_sub_status_ = false;
-    image_sub_status_ = false; 
+    camera_info_sub_status_ = false;
 }
 
 ImageCartesianMapperNode::~ImageCartesianMapperNode()
 {
     event_sub_.shutdown();
     pose_sub_.shutdown();
-    image_sub_.shutdown();
+    camera_info_sub_.shutdown();
     cartesian_pub_.shutdown();
     event_pub_.shutdown();  
 }
-
 
 
 void ImageCartesianMapperNode::eventCallback(const std_msgs::String &event_command)
 {
     if (event_command.data == "e_start") {
         start_cartesian_mapper_ = true;
-        ROS_INFO("Image to Cartesian Mapper ENABLED");
+        ROS_INFO("ENABLED");
     } else if (event_command.data == "e_stop") {
         start_cartesian_mapper_ = false;
-        ROS_INFO("Image to Cartesian Mapper DISABLED");
+        ROS_INFO("DISABLED");
     }
 }
 
@@ -41,14 +44,11 @@ void ImageCartesianMapperNode::poseCallback(const geometry_msgs::Pose2D::ConstPt
     pose_sub_status_ = true;
 }
 
-void ImageCartesianMapperNode::imageCallback(const sensor_msgs::ImageConstPtr &image)
+void ImageCartesianMapperNode::cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr &camera_info)
 {
-    image_message_ = image;
-    image_sub_status_ = true;
-
+    camera_info_ = camera_info;
+    camera_info_sub_status_ = true;
 }
-
-
 
 void ImageCartesianMapperNode::states()
 {
@@ -69,10 +69,10 @@ void ImageCartesianMapperNode::states()
 
 void ImageCartesianMapperNode::initState()
 {
-    if (pose_sub_status_ && image_sub_status_) {
+    if (pose_sub_status_ && camera_info_sub_status_) {
         run_state_ = IDLE;
         pose_sub_status_ = false;
-        image_sub_status_ = false;
+        camera_info_sub_status_ = false;
     }
 }
 
@@ -87,49 +87,69 @@ void ImageCartesianMapperNode::idleState()
 
 void ImageCartesianMapperNode::runState()
 {
-    imagetoCartesianMapper();
+
+    if(cameraOpticalToCameraMetrical()){
+        if(cameraMetricalToCartesian()){
+            cartesian_pub_.publish(cartesian_pose_);    
+        } else {
+            event_out_msg_.data = "e_error";
+            event_pub_.publish(event_out_msg_);
+        }
+    }
+
     run_state_ = INIT;
 }
 
-void ImageCartesianMapperNode::imagetoCartesianMapper()
+
+bool ImageCartesianMapperNode::cameraOpticalToCameraMetrical()
 {
 
-    cv_bridge::CvImagePtr cv_img_ptr;
-    
-    try {
-        cv_img_ptr = cv_bridge::toCvCopy(image_message_, sensor_msgs::image_encodings::BGR8);
-    } catch (cv_bridge::Exception &e) {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", image_message_->encoding.c_str());
-        return;
-    }
+    camera_coordinates_ << pose_2d_.x, pose_2d_.y, 1;
 
-    Size cv_img_size = cv_img_ptr->image.size();
-    center_x_ = cv_img_size.width/2;
-    center_y_ = cv_img_size.height/2;
+    camera_intrinsic_matrix_ << camera_info_->K[0], camera_info_->K[1], camera_info_->K[2], 
+                                camera_info_->K[3], camera_info_->K[4], camera_info_->K[5], 
+                                camera_info_->K[6], camera_info_->K[7], camera_info_->K[8];
+
+    camera_metrical_coordinates_ = camera_intrinsic_matrix_.inverse()*camera_coordinates_;
 
     geometry_msgs::Pose pose;
     geometry_msgs::Point point;
     geometry_msgs::Quaternion quaternion;
 
-    point.x = pose_2d_.x - center_x_;
-    point.y = pose_2d_.y - center_y_;
+    point.x = camera_metrical_coordinates_(0,0);
+    point.y = camera_metrical_coordinates_(1,0);
+    point.z = camera_metrical_coordinates_(2,0);
     quaternion = tf::createQuaternionMsgFromYaw(pose_2d_.theta*PI/180);
     pose.position = point;
     pose.orientation = quaternion;
 
-    cartesian_pose_.header.stamp = ros::Time::now();
-    cartesian_pose_.header.frame_id = "/tower_cam3d_rgb_optical_frame";
-    cartesian_pose_.pose = pose;
+    camera_optical_pose_.header.stamp = ros::Time::now();
+    camera_optical_pose_.header.frame_id = source_frame_;
+    camera_optical_pose_.pose = pose;
 
-    cartesian_pub_.publish(cartesian_pose_);
+    return true;
+}
 
+
+bool ImageCartesianMapperNode::cameraMetricalToCartesian()
+{
+    try{
+        listener_.waitForTransform(target_frame_, camera_optical_pose_.header.frame_id, camera_optical_pose_.header.stamp, ros::Duration(3.0));
+        listener_.transformPose(target_frame_, camera_optical_pose_, cartesian_pose_);
+        return true;
+    }
+    catch (tf::TransformException ex) {
+        ROS_ERROR("%s",ex.what());
+        return false;
+    }
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_cartesian_mapper");
     ros::NodeHandle nh("~");
-    ROS_INFO("Image to Cartesian Mapper Node Initialised");
+    ROS_INFO("Initialised");
+
     ImageCartesianMapperNode icm(nh);
     while (ros::ok()) {
         icm.states();
