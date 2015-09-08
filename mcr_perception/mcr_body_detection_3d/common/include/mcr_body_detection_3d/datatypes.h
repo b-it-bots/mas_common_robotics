e6099a27176b9adc93fbd5cd99a93458c4755ed4
Frederik Hegger <frederik.hegger@h-brs.de>
2015-09-08 14:50:14 +0000

    [body detection 3d] replace tabs by spaces

--- mcr_perception/mcr_body_detection_3d/common/include/mcr_body_detection_3d/datatypes.h
+++ mcr_perception/mcr_body_detection_3d/common/include/mcr_body_detection_3d/datatypes.h
@@ -10,45 +10,45 @@
 
 struct Person
 {
-	Person() : position_x(0.0), position_y(0.0), position_z(0.0), orientation_yaw(0.0), height(0.0), width(0.0), depth(0.0), probability(0.0) {};
-	double position_x;
-	double position_y;
-	double position_z;
-	double orientation_yaw;
-	double height;
-	double width;
-	double depth;
-	double probability;
+    Person() : position_x(0.0), position_y(0.0), position_z(0.0), orientation_yaw(0.0), height(0.0), width(0.0), depth(0.0), probability(0.0) {};
+    double position_x;
+    double position_y;
+    double position_z;
+    double orientation_yaw;
+    double height;
+    double width;
+    double depth;
+    double probability;
 };
 
 struct Segment3D
 {
-	Segment3D() : number_of_segments(0), probability(0.0) {};
-	pcl::PointCloud<pcl::PointNormal> pcl_cloud;
-	unsigned int number_of_segments;
-	double probability;
+    Segment3D() : number_of_segments(0), probability(0.0) {};
+    pcl::PointCloud<pcl::PointNormal> pcl_cloud;
+    unsigned int number_of_segments;
+    double probability;
 };
 
 struct Segment3DProperties
 {
-	Segment3DProperties() : probability(0.0) {};
-	pcl::PointXYZ centroid;
-	double probability;
+    Segment3DProperties() : probability(0.0) {};
+    pcl::PointXYZ centroid;
+    double probability;
 };
 
 struct Vertex
 {
-	Vertex() : related_segment_id(0), id(0), probability(0.0) {};
-	pcl::PointXYZ point;
-	unsigned int related_segment_id;
-	unsigned int id;
-	double probability;
+    Vertex() : related_segment_id(0), id(0), probability(0.0) {};
+    pcl::PointXYZ point;
+    unsigned int related_segment_id;
+    unsigned int id;
+    double probability;
 };
 
 struct Edge
 {
-	Edge() : eucl_distance(0.0) {};
-	double eucl_distance;
+    Edge() : eucl_distance(0.0) {};
+    double eucl_distance;
 };
 
 
