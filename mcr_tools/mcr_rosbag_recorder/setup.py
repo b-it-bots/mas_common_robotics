2df813f21640b585caa36a0f4c805ed4f68cfdbe
Frederik Hegger <frederik.hegger@h-brs.de>
2016-02-16 14:22:16 +0100

    [rosbag_recorder] fix setup.py for indigo

--- mcr_tools/mcr_rosbag_recorder/setup.py
+++ mcr_tools/mcr_rosbag_recorder/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['mcr_rosbag_recorder'],
-    package_dir={'mcr_rosbag_recorder': 'ros/src'}
+    package_dir={'mcr_rosbag_recorder': 'ros/src/mcr_rosbag_recorder'}
 )
 
 setup(**d)
