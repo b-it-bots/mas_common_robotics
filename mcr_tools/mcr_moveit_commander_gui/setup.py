8bae5f45e765295a23de58d1344f4872d675ea43
Frederik Hegger <frederik.hegger@h-brs.de>
2016-02-17 08:07:03 +0100

    [moveit commander gui] fix setup.py for indigo

--- mcr_tools/mcr_moveit_commander_gui/setup.py
+++ mcr_tools/mcr_moveit_commander_gui/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['mcr_moveit_commander_gui'],
-    package_dir={'mcr_moveit_commander_gui': 'ros/src'},
+    package_dir={'mcr_moveit_commander_gui': 'ros/src/mcr_moveit_commander_gui'},
     requires=['rospy']
 )
 
