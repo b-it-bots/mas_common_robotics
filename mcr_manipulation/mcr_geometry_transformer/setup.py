183f93e04484cbeee4060f97b5fd3372ad3a5119
Frederik Hegger <frederik.hegger@h-brs.de>
2016-02-26 19:29:36 +0100

    [geometry_transformer] fix directories

--- mcr_manipulation/mcr_geometry_transformer/setup.py
+++ mcr_manipulation/mcr_geometry_transformer/setup.py
@@ -5,8 +5,7 @@ import catkin_pkg.python_setup
 
 d = catkin_pkg.python_setup.generate_distutils_setup(
    packages=['geometry_transformer_util', 'mcr_geometry_transformer'],
-   package_dir={'geometry_transformer_util': 'common/scripts/geometry_transformer_util',
+   package_dir={'geometry_transformer_util': 'common/src/geometry_transformer_util',
                 'mcr_geometry_transformer': 'ros/src/mcr_geometry_transformer'}
 )
 
-distutils.core.setup(**d)
