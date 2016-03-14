7dc055f68616549361c913e8e5dfb545577a94c5
Shehzad Ahmed <shehzi001@gmail.com>
2016-03-14 19:05:14 +0100

    [mcr_guarded_approach_pose] fix python package base name in python setup

--- mcr_manipulation/mcr_guarded_approach_pose/setup.py
+++ mcr_manipulation/mcr_guarded_approach_pose/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['mcr_guarded_approach_pose_ros'],
-    package_dir={'mcr_guarded_approach_pose_ros': 'ros/src'}
+    package_dir={'mcr_guarded_approach_pose_ros': 'ros/src/mcr_guarded_approach_pose_ros'}
 )
 
 setup(**d)
