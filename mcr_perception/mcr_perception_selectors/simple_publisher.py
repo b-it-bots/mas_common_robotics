e203aad5271f3dafd6d8215cae1964458b4fad0f
Padmaja <padmaja.kulkarni@smail.inf.h-brs.de>
2016-06-20 15:16:20 +0200

    Cavity name publisher to test the working of the node.

---
+++ mcr_perception/mcr_perception_selectors/simple_publisher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+import mcr_perception_msgs.msg as mpm
+import std_msgs.msg
+import numpy as np
+
+def talker():
+    pub = rospy.Publisher('/mcr_perception/cavity_pose_publisher/cavity',\
+     mpm.Cavity, queue_size=10)
+    rospy.init_node('simple_publisher', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    i = 0
+    msg = mpm.Cavity()
+    msg2 = mpm.Cavity()
+    ###Filling the message
+    msg.name ='F20_20'
+    msg.object_name = 'F20_20_G'
+    msg.pose.pose.position.x = 10
+    msg2.name ='S40_40'
+    msg2.object_name = 'S40_40_G'
+    msg2.pose.pose.position.x = 20
+    while not rospy.is_shutdown():
+        if i ==0:
+            pub.publish(msg)
+        if i ==20:
+            pub.publish(msg2)
+        i = i+1
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
