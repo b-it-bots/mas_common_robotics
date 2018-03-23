4b7051308dd79115c2a2715ed10303b991c9cf9a
aaqib49 <aaqib49@gmail.com>
2018-03-23 21:11:57 +0100

    Modified DBC files to run on a single node (#13)
    * Modified DBC files to run on a single node
    
    * delete component wise pose error monitor and dbc standalone launch file
    
    * delete dbc standalone launch file
    
    * Updated the code as per comments from santosh

--- mcr_controllers/mcr_twist_limiter/ros/src/mcr_twist_limiter_ros/twist_limiter.py
+++ mcr_controllers/mcr_twist_limiter/ros/src/mcr_twist_limiter_ros/twist_limiter.py
@@ -21,10 +21,6 @@ class TwistLimiter(object):
 
     """
     def __init__(self):
-        # params
-        self.event = None
-        self.twist = None
-
         # Maximum linear velocities in the axes X, Y and Z (in meters/second)
         self.max_velocity_x = rospy.get_param('~max_velocity_x', 0.1)
         self.max_velocity_y = rospy.get_param('~max_velocity_y', 0.1)
@@ -33,111 +29,23 @@ class TwistLimiter(object):
         # Maximum angular velocities around the axes X, Y and Z (in radians/second)
         self.max_velocity_roll = rospy.get_param('~max_velocity_roll', 0.1)
         self.max_velocity_pitch = rospy.get_param('~max_velocity_pitch', 0.1)
-        self.max_velocity_yaw = rospy.get_param('~max_velocity_yaw', 0.1)
+        self.max_velocity_yaw = rospy.get_param('~max_velocity_yaw', 0.6)
 
-        # node cycle rate (in hz)
-        self.loop_rate = rospy.Rate(rospy.get_param('~loop_rate', 10))
-
-        # publishers
-        self.limited_twist = rospy.Publisher(
-            '~limited_twist', geometry_msgs.msg.TwistStamped, queue_size=5
-        )
-        self.event_out = rospy.Publisher('~event_out', std_msgs.msg.String, queue_size=5)
-
-        # subscribers
-        rospy.Subscriber('~event_in', std_msgs.msg.String, self.event_in_cb)
-        rospy.Subscriber('~twist', geometry_msgs.msg.TwistStamped, self.twist_cb)
-
-    def start(self):
+    def set_parameters(self, max_velocity_x, max_velocity_y, max_velocity_z,\
+        max_velocity_yaw, max_velocity_pitch, max_velocity_roll):
         """
-        Starts the component.
-
+        Helper method for dynamic reconfiguration
         """
-        rospy.loginfo("Ready to start...")
-        state = 'INIT'
+        self.max_velocity_x = max_velocity_x
+        self.max_velocity_y = max_velocity_y
+        self.max_velocity_z = max_velocity_z
+        self.max_velocity_yaw = max_velocity_yaw
+        self.max_velocity_roll = max_velocity_roll
+        self.max_velocity_pitch = max_velocity_pitch
 
-        while not rospy.is_shutdown():
 
-            if state == 'INIT':
-                state = self.init_state()
-            elif state == 'IDLE':
-                state = self.idle_state()
-            elif state == 'RUNNING':
-                state = self.running_state()
 
-            rospy.logdebug("State: {0}".format(state))
-            self.loop_rate.sleep()
-
-    def event_in_cb(self, msg):
-        """
-        Obtains an event for the component.
-
-        """
-        self.event = msg.data
-
-    def twist_cb(self, msg):
-        """
-        Obtains the twist.
-
-        """
-        self.twist = msg
-
-    def init_state(self):
-        """
-        Executes the INIT state of the state machine.
-
-        :return: The updated state.
-        :rtype: str
-
-        """
-        if self.event == 'e_start':
-            return 'IDLE'
-        else:
-            return 'INIT'
-
-    def idle_state(self):
-        """
-        Executes the IDLE state of the state machine.
-
-        :return: The updated state.
-        :rtype: str
-
-        """
-        if self.event == 'e_stop':
-            self.event = None
-            self.twist = None
-            return 'INIT'
-        elif self.twist:
-            return 'RUNNING'
-        else:
-            return 'IDLE'
-
-    def running_state(self):
-        """
-        Executes the RUNNING state of the state machine.
-
-        :return: The updated state.
-        :rtype: str
-
-        """
-        if self.event == 'e_stop':
-            self.event = None
-            self.twist = None
-            self.event_out.publish('e_stopped')
-            return 'INIT'
-        else:
-            limited_twist = self.limit_twist()
-            if limited_twist:
-                self.limited_twist.publish(limited_twist)
-                self.event_out.publish('e_success')
-            else:
-                self.event_out.publish('e_failure')
-
-            self.event = None
-            self.twist = None
-            return 'IDLE'
-
-    def limit_twist(self):
+    def get_limited_twist(self, twist):
         """
         Limits a twist if it exceeds the specified maximum.
 
@@ -145,6 +53,7 @@ class TwistLimiter(object):
         :rtype: geometry_msgs.msg.TwistStamped
 
         """
+        self.twist = twist
         limited_twist = geometry_msgs.msg.TwistStamped()
         limited_twist.header.frame_id = self.twist.header.frame_id
         limited_twist.header.stamp = self.twist.header.stamp
@@ -167,11 +76,4 @@ class TwistLimiter(object):
         limited_twist.twist.angular.z = limiter.limit_value(
             self.twist.twist.angular.z, self.max_velocity_yaw
         )
-
         return limited_twist
-
-
-def main():
-    rospy.init_node('twist_limiter', anonymous=True)
-    twist_limiter = TwistLimiter()
-    twist_limiter.start()
