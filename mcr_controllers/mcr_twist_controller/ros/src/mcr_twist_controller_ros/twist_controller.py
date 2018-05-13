0dca0658980220804fbd6bdca8ea70d1a12832b0
Santosh Thoduka <santosh.thoduka@gmail.com>
2018-05-13 19:15:43 +0200

    [twist controller] roslint check and update controller with new parameters

--- mcr_controllers/mcr_twist_controller/ros/src/mcr_twist_controller_ros/twist_controller.py
+++ mcr_controllers/mcr_twist_controller/ros/src/mcr_twist_controller_ros/twist_controller.py
@@ -4,14 +4,15 @@ This module contains a component that calculates a
 twist to reduce the difference between two poses.
 
 """
-#-*- encoding: utf-8 -*-
-__author__ = 'jsanch'
 
 import rospy
 import std_msgs.msg
 import geometry_msgs.msg
 import mcr_manipulation_msgs.msg
 import mcr_algorithms.controller.pid_controller as pid_controller
+
+# -*- encoding: utf-8 -*-
+__author__ = 'jsanch'
 
 # This value is used as the 'current_value' for the 'pid_controller'
 # from 'mcr_algorithms.controller.pid_controller', since this twist
@@ -50,6 +51,13 @@ class TwistController(object):
         self.p_gain_roll = p_gain_roll
         self.p_gain_pitch = p_gain_pitch
         self.p_gain_yaw = p_gain_yaw
+
+        self.x_controller = pid_controller.p_controller(self.p_gain_x)
+        self.y_controller = pid_controller.p_controller(self.p_gain_y)
+        self.z_controller = pid_controller.p_controller(self.p_gain_z)
+        self.roll_controller = pid_controller.p_controller(self.p_gain_roll)
+        self.pitch_controller = pid_controller.p_controller(self.p_gain_pitch)
+        self.yaw_controller = pid_controller.p_controller(self.p_gain_yaw)
 
     def get_cartesian_velocity(self, pose_error):
         """
